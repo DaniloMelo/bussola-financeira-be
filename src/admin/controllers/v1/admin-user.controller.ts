b6505de73f0a640e49392d7b7c6ba45a892a2c65
DaniloMelo <danilomarquesdemelo@gmail.com>
2026-01-20 12:40:38 -0300

    docs(swagger): add documentation for /v1/admin (GET) endpoint

--- src/admin/controllers/v1/admin-user.controller.ts
+++ src/admin/controllers/v1/admin-user.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get, Query, UseGuards } from "@nestjs/common";
-import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from "@nestjs/swagger";
 import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
 import { Roles } from "src/common/decorators/roles.decorator";
 import { Role } from "src/common/enums/role.enum";
 import { RolesGuard } from "src/common/guards/roles.guard";
 import { PaginationDtoV1 } from "./dto/pagination.dto";
 import { AdminUserService } from "src/admin/services/admin-user.service";
+import { AdminFindAllApiResponseDtoV1 } from "./dto/swagger/admin-find-all-api-response.dto";
 
 @Controller({ path: "admin", version: "1" })
 @ApiTags("admin-v1")
@@ -15,7 +21,13 @@ export class AdminController {
   @Get()
   @Roles(Role.ADMIN)
   @UseGuards(JwtAuthGuard, RolesGuard)
+  @ApiOperation({ summary: "Lista todos os usuários" })
   @ApiBearerAuth("access-token")
+  @ApiResponse({
+    status: 200,
+    description: "Retorna a lista de usuários",
+    type: AdminFindAllApiResponseDtoV1,
+  })
   findAll(@Query() pagination: PaginationDtoV1) {
     return this.adminUserService.findAll(pagination);
   }
